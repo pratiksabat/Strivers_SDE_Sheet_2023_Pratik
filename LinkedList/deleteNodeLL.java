9b1f8eb73532850d2fe44a292f7bff74f50dba5b
Pratik Sabata <47950768+pratiksabat@users.noreply.github.com>
2023-06-13 20:45:39 +0530

    Solved the qn, by copying the next node's data to curr, and move node.next to node.next.next

---
+++ LinkedList/deleteNodeLL.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.* ;
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+        LinkedListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*****************************************************************/
+
+public class Solution {
+	public static void deleteNode(LinkedListNode<Integer> node) {
+		// Write your code here.
+        // System.out.println(node.data);
+        node.data=node.next.data;
+        node.next=node.next.next;
+        // node=node.next;
+	}
+}
